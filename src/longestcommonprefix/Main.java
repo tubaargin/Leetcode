7a1129ff973fb946d507ebba4474d9f7d014c1fe
Tuba <tubargin1995@gmail.com>
2026-03-28 20:17:51 +0300

    longestCommonPrefix

---
+++ src/longestcommonprefix/Main.java
@@ -0,0 +1,8 @@
+package longestcommonprefix;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(Solution.longestCommonPrefix(strs));
+    }
+}
